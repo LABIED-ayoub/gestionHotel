31439df8544b4b28f8f3ca4a377c5b458f9cb913
LABIED-ayoub <94143493+LABIED-ayoub@users.noreply.github.com>
2022-04-12 19:37:39 -0300

    Initial commit

---
+++ src/test/java/com/example/gestionHotels/GestionHotelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionHotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionHotelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
